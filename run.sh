db7061eb9ff12a1fb03bc9dc375e4430d7f7ed11
Chenhui1016 <cd574@cornell.edu>
2024-03-19 14:40:23 -0400

    update hyper configs on small graphs

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ python main.py --dataset questions --hidden_channels 64 --local_epochs 200 --glo
 ## homophilic datasets
 python main.py --dataset amazon-computer --hidden_channels 64 --local_epochs 200 --global_epochs 1000 --lr 0.001 --runs 1 --local_layers 5 --global_layers 1 --weight_decay 5e-5 --dropout 0.7 --in_dropout 0.2 --num_heads 8 --device 0 --save_model
 python main.py --dataset amazon-photo --hidden_channels 64 --local_epochs 200 --global_epochs 1000 --lr 0.001 --runs 1 --local_layers 7 --global_layers 2 --weight_decay 5e-5 --dropout 0.7 --in_dropout 0.2 --num_heads 8 --device 0 --save_model
-python main.py --dataset coauthor-cs --hidden_channels 64 --local_epochs 100 --global_epochs 1500 --lr 0.001 --runs 1 --local_layers 5 --global_layers 2 --weight_decay 5e-4 --dropout 0.3 --in_dropout 0.1 --num_heads 8 --device 0 --save_model &
-python main.py --dataset coauthor-physics --hidden_channels 32 --local_epochs 100 --global_epochs 1500 --lr 0.001 --runs 1 --local_layers 5 --global_layers 4 --weight_decay 5e-4 --dropout 0.5 --in_dropout 0.1 --num_heads 8 --device 2 --save_model &
-python main.py --dataset wikics --hidden_channels 512 --local_epochs 100 --global_epochs 1000 --lr 0.001 --runs 1 --local_layers 7 --global_layers 2 --weight_decay 0.0 --dropout 0.5 --in_dropout 0.5 --num_heads 1 --device 3 --save_model &
+python main.py --dataset coauthor-cs --hidden_channels 64 --local_epochs 100 --global_epochs 1500 --lr 0.001 --runs 1 --local_layers 5 --global_layers 2 --weight_decay 5e-4 --dropout 0.3 --in_dropout 0.1 --num_heads 8 --device 0 --save_model
+python main.py --dataset coauthor-physics --hidden_channels 32 --local_epochs 100 --global_epochs 1500 --lr 0.001 --runs 1 --local_layers 5 --global_layers 4 --weight_decay 5e-4 --dropout 0.5 --in_dropout 0.1 --num_heads 8 --device 2 --save_model
+python main.py --dataset wikics --hidden_channels 512 --local_epochs 100 --global_epochs 1000 --lr 0.001 --runs 1 --local_layers 7 --global_layers 2 --weight_decay 0.0 --dropout 0.5 --in_dropout 0.5 --num_heads 1 --device 3 --save_model
 
