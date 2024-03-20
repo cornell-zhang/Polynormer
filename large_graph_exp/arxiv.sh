ebc29b94d1c758f283dac363b41b46bd5b0a15ac
Chenhui1016 <cd574@cornell.edu>
2024-03-20 19:43:25 -0400

    update device id

--- large_graph_exp/arxiv.sh
+++ large_graph_exp/arxiv.sh
@@ -1,4 +1,2 @@
-GPU=1
-
-python main-arxiv.py --dataset ogbn-arxiv --hidden_channels 256 --local_epochs 2000 --global_epochs 0 --lr 0.0005 --runs 1 --local_layers 7 --global_layers 2 --device $GPU --post_bn
+python main-arxiv.py --dataset ogbn-arxiv --hidden_channels 256 --local_epochs 2000 --global_epochs 0 --lr 0.0005 --runs 1 --local_layers 7 --global_layers 2 --post_bn --device 0
 
