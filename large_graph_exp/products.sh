7c66d9c61118af193c17898e1d00ebe32aa0ec2b
Chenhui1016 <cd574@cornell.edu>
2024-03-20 17:27:15 -0400

    update products epochs

--- large_graph_exp/products.sh
+++ large_graph_exp/products.sh
@@ -1,4 +1,4 @@
 GPU=1
 
-python main-batch.py --dataset ogbn-products --local_attn --hidden_channels 32 --num_heads 8 --local_epochs 500 --global_epochs 0 --lr 0.0005 --batch_size 100000 --runs 1 --local_layers 10 --global_layers 2 --device $GPU --pre_ln --post_bn --in_drop 0.2 --weight_decay 0.0 --eval_step 9 --eval_epoch 300
+python main-batch.py --dataset ogbn-products --local_attn --hidden_channels 32 --num_heads 8 --local_epochs 1000 --global_epochs 0 --lr 0.0005 --batch_size 100000 --runs 1 --local_layers 10 --global_layers 2 --device $GPU --pre_ln --post_bn --in_drop 0.2 --weight_decay 0.0 --eval_step 9 --eval_epoch 500
 
